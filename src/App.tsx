27701cd8c671a3ec711f1854a66fadc82d48b379
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2025-05-25 20:49:23 -0300

    feat: header (#2)
    * feat: add default layout
    
    * feat: add header
    
    * refactor: remove easter-eggs section
    
    * chore: solve packages vulnerabilities

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import { useState, useEffect } from "react";
-import Header from "./components/Header";
 import Countdown from "./components/Countdown";
 import AboutSection from "./components/AboutSection";
 import LettersSection from "./components/LettersSection";
-import EasterEggSection from "./components/EasterEggs";
-import Footer from "./components/Footer";
 import LoadingPage from "./view/components/LoadingPage";
-import { Heart } from "lucide-react";
-import { easterEggs } from "./data/easterEggs";
+import { DefaultLayout } from "./view/layout/Default";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -22,34 +18,13 @@ function App() {
 
   return (
     <div className="min-h-screen bg-pink-50">
-      {isLoading ? (
-        <LoadingPage onLoadingComplete={handleLoadingComplete} />
-      ) : (
-        <>
-          <Header />
+      {isLoading && <LoadingPage onLoadingComplete={handleLoadingComplete} />}
+      {!isLoading && (
+        <DefaultLayout>
           <Countdown />
           <AboutSection />
           <LettersSection />
-          <EasterEggSection />
-          <Footer />
-          <div
-            id="hidden-heart-icon"
-            className="fixed bottom-10 right-2 opacity-10 hover:opacity-100 cursor-pointer transition-opacity"
-            onClick={() => {
-              const heartEgg = easterEggs.find(
-                (egg) => egg.id === "hidden-heart"
-              );
-              if (heartEgg) {
-                const event = new CustomEvent("eggFound", {
-                  detail: { id: heartEgg.id, message: heartEgg.message },
-                });
-                window.dispatchEvent(event);
-              }
-            }}
-          >
-            <Heart className="w-6 h-6 text-pink-600" fill="pink" />
-          </div>
-        </>
+        </DefaultLayout>
       )}
     </div>
   );
